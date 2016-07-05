24c40828f1f2e10c779dc1b8498f5d59c167064d
Benjamin Dant <benjamindant@gmail.com>
2016-07-05 17:28:24 +0200

    Ajout du layout principal des pages

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Citycom</title>
+</head>
+<body>
+	@include('parts.header')
+	
+	
+
+	@include('parts.footer')
+</body>
+</html>
