85bb69678daeaa3830e6b434cbbb6be298958074
Hayate Sehli <sehli.hayate@live.fr>
2016-07-05 16:58:26 +0200

    installation et intégration de sempantic ui

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="../vendor/wangaz/laravel-semantic-ui-css/src/assets/semantic-ui/semantic.min.css">
 	<title>Le royaume de la caillasse</title>
 </head>
 <body>
