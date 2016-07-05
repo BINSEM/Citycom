f484f52e0709db6c2add7f6df56aabe310c3ebac
Hayate Sehli <sehli.hayate@live.fr>
2016-07-05 16:21:50 +0200

    formulaire d'ajout d'article

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('article.ajoutArticle');
 });
 
 Route::auth();
---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Le royaume de la caillasse</title>
+</head>
+<body>
+	
