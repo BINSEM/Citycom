48ef30f8b2e62d7a374184ec5c552c2c4ad6f6fe
binsem <binsem@bitbucket.org>
2016-07-08 23:17:00 +0200

    modification de images.blade.php

--- resources/views/user/images.blade.php
+++ resources/views/user/images.blade.php
@@ -2,18 +2,17 @@
 
 @section('content')
 
-<div id="adresse" class="ui grid" style="margin-top:6.5rem; width: 95%; margin-left: auto; margin-right: auto;">
+<div id="adresse" class="ui centered grid" style="margin-top:6.5rem; width: 95%; margin-left: auto; margin-right: auto;">
+	<h2>Vos images de profil</h2>
 	<div class="doubling four column row">
 		@foreach($user->images as $img)
 		<div class="four wide column" style="margin-bottom: 1rem">
 			<div class="ui card">
-				<div class="content">
-					<a href="/profile/image/{{$img->id}}" class="ui basic green button">Faire profil l'image </a>
-				</div>
 				<div class="image">
 					<img src="{{ $img->path }}" style="height: 180px;">
 				</div>
 			</div>
+			<a href="/profile/image/{{$img->id}}" class="ui yellow button">Mettre cette image en profil</a>
 		</div>
 		@endforeach
 	</div>
