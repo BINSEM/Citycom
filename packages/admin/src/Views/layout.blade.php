289ac1b76a45887a231c320944fb413ba266097a
Benjamin Dant <benjamindant@gmail.com>
2016-07-10 01:00:51 +0200

    Mise en forme des vues administrateur

--- packages/admin/src/Views/layout.blade.php
+++ packages/admin/src/Views/layout.blade.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
 	<title>Panel Administrateur</title>
 	@include('parts.header')
 	<link rel="stylesheet" href="/assets/css/admin.css">
@@ -9,7 +11,7 @@
 <body>
 	<nav class="ui breadcrumb">
 		<a href="/admin">Administration</a>
-		<span class="divider">|</span>
+		<span class="divider"> | </span>
 		<a href="/">Retour au magasin</a>
 	</nav>
 	<section class="ui padded grid">
