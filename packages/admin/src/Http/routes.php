92246546966682e891715d886028202caa40ce29
Benjamin Dant <benjamindant@gmail.com>
2016-07-10 22:46:11 +0200

    Finalisation du panier

--- packages/admin/src/Http/routes.php
+++ packages/admin/src/Http/routes.php
@@ -18,3 +18,36 @@ Route::group(['middleware' => ['web', 'admin'], 'prefix' => 'admin'], function (
 Route::get('/magasin', function(){
 	return 'Magasin';
 });
+Route::post('/panier', function(){
+		$basket = array();
+		if(session()->has('cart')){
+			$cart = session()->get('cart');
+			foreach ($cart as $key => $value) {
+				array_push($basket, $value['id']);
+			}
+		}
+		$detail = array();
+		for ($i = 0; $i < count($basket); $i++){
+			if(array_key_exists($basket[$i], $detail)){
+				$detail[$basket[$i]]++;
+			} else {
+				$detail[$basket[$i]] = 1;
+			}
+		}
+		$basket = implode(',', $basket);
+		if(Auth::check()) {
+			$user = Auth::user();
+			$order = new Order;
+			$order->user_id = $user->id;
+			$order->commande = $basket;
+			$order->save();
+			session()->forget('cart');
+		} else {
+			session()->flash('message', 'Inscrivez vous pour poursuivre vos achats.');
+			return redirect('/register');
+		}
+		return redirect('/commande');
+	});
+Route::get('/commande', function(){
+	return view('order::confirmer');
+});
