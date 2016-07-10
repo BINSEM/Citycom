5dcc70321fd8247c183ea6f04be693c3af8d7af0
binsem <binsem@bitbucket.org>
2016-07-10 16:36:09 +0200

    modifiction du liens pour le panier

--- resources/views/parts/navbar.blade.php
+++ resources/views/parts/navbar.blade.php
@@ -12,81 +12,81 @@
 					<a class="item">Petits Cailloux</a>
 				</div>
 			</div>
-			<a class="item">
+			<a class="item" href="/cart">
 				<i class="shop icon"></i>
 				Panier
-				<br>@if (isset($carts)) {{$carts}} Produits {{$total}} &euro; @endif</a>
-				<div class="right menu">
+			</a>
+			<div class="right menu">
 
-					@if (Auth::guest())
-					<form class="ui form">
-						<div class="item">
-							<div class="shake-chunk">
-								<input type="button" onClick="parent.location='/register'" class="ui fluid yellow button" value="Inscription" />
-							</div> 
-							&nbsp;
-							&nbsp;
-							<div class="shake-little">
-								<input type="button" onClick="parent.location='/login'" class="ui fluid yellow button" value="Connection" />
-							</div>
+				@if (Auth::guest())
+				<form class="ui form">
+					<div class="item">
+						<div class="shake-chunk">
+							<input type="button" onClick="parent.location='/register'" class="ui fluid yellow button" value="Inscription" />
+						</div> 
+						&nbsp;
+						&nbsp;
+						<div class="shake-little">
+							<input type="button" onClick="parent.location='/login'" class="ui fluid yellow button" value="Connection" />
 						</div>
-						@else
-						<div class="item">
-							<input type="button" onClick="parent.location='/logout'" class="ui fluid teal button" value="Déconnection" />
-						</div>
-					</form>
-					@endif
+					</div>
+					@else
+					<div class="item">
+						<input type="button" onClick="parent.location='/logout'" class="ui fluid teal button" value="Déconnection" />
+					</div>
+				</form>
+				@endif
 
-				</div>
 			</div>
 		</div>
 	</div>
+</div>
 
-	<div class="ui stackable grid">
-		<div class="mobile only row">
-			<div class="ui fixed menu">
-				<div class="ui vertical menu" >
-					<div class="ui menu ori ">
-						<a class="item" href="/">Accueil</a>
-						<div class="ui dropdown item">
-							<i class="dropdown icon"></i>
-							Catégories
-							<div class="ui menu">
-								<a class="item">Gros Cailloux</a>
-								<a class="item">Moyens Cailloux</a>
-								<a class="item">Petits Cailloux</a>
-							</div>
+<div class="ui stackable grid">
+	<div class="mobile only row">
+		<div class="ui fixed menu">
+			<div class="ui vertical menu" >
+				<div class="ui menu ori ">
+					<a class="item" href="/">Accueil</a>
+					<div class="ui dropdown item">
+						<i class="dropdown icon"></i>
+						Catégories
+						<div class="ui menu">
+							<a class="item">Gros Cailloux</a>
+							<a class="item">Moyens Cailloux</a>
+							<a class="item">Petits Cailloux</a>
 						</div>
-						<a class="item">Panier</a>
-						<div class="right menu">
+					</div>
+					<a class="item">Panier</a>
+					<div class="right menu">
 
-							@if (Auth::guest())
-							<form class="ui form">
-								<div class="item">
-									<div class="ui vertical buttons">
-										<div class="shake-chunk">
-											<input type="button" onClick="parent.location='/register'" class="ui mini yellow button" value="Inscription" />
-										</div> 
-										&nbsp;
-										&nbsp;
-										<div class="shake-little">
-											<input type="button" onClick="parent.location='/login'" class="ui fluid mini yellow button" value="Connection" />
-										</div>
+						@if (Auth::guest())
+						<form class="ui form">
+							<div class="item">
+								<div class="ui vertical buttons">
+									<div class="shake-chunk">
+										<input type="button" onClick="parent.location='/register'" class="ui mini yellow button" value="Inscription" />
+									</div> 
+									&nbsp;
+									&nbsp;
+									<div class="shake-little">
+										<input type="button" onClick="parent.location='/login'" class="ui fluid mini yellow button" value="Connection" />
 									</div>
 								</div>
-								@else
-								<div class="item">
-									<input type="button" onClick="parent.location='/logout'" class="ui fluid mini yellow button" value="Déconnection" />
-								</div>
-							</form>
-							@endif
+							</div>
+							@else
+							<div class="item">
+								<input type="button" onClick="parent.location='/logout'" class="ui fluid mini yellow button" value="Déconnection" />
+							</div>
+						</form>
+						@endif
 
-						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
 
 
 
