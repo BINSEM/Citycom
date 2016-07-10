928b98638c8e1527e7ea0ce31bd772e7fd03e8b6
Benjamin Dant <benjamindant@gmail.com>
2016-07-10 21:46:18 +0200

    Modification du JS faisant disparaître le gros titre de la page d'accueil

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,10 +4,16 @@ $(document).ready(function(){
 	console.log('sourced app.js');
 	var visited = localStorage.getItem('visited');
 	console.log(window.location);
-	if(visited){
-		if (window.location.pathname == "/"){
-			window.location = "/cart/test";
+		var message = document.getElementById('message');
+		console.log(typeof $message);
+		if (message) {
+			message.style.margin = '6em 0 0 0';
 		}
+	if(visited){
+		document.getElementById('text').remove();
+		document.getElementById('centertitre').innerHTML = '<h3>Au Royaume de la Caillasse</h3>';
+		document.getElementById('centertitre').style.margin = '4em 0 0 0';
+		document.getElementById('centertitre').style.padding = '1em 0 0 0';
 	} else {
 		localStorage.setItem('visited', true);
 	}
