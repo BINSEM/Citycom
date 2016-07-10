6191de8da2e53413688dd6d677ef0496cee2a6f4
binsem <binsem@bitbucket.org>
2016-07-10 20:17:03 +0200

    ajouter des database et modification

--- resources/views/cart/show.blade.php
+++ resources/views/cart/show.blade.php
@@ -14,12 +14,11 @@
 			<tbody>
 				@foreach($carts as $cart)
 				<tr>
-					<td><img src="{{$cart['img']}}" alt="pomme" width="64" height="64"></td>
+					<td><img src="{{$cart['img']}}" alt="{{$cart['nom']}}" width="64" height="64"></td>
 					<td class="center aligned">{{$cart['nom']}}</td>
 					<td class="right aligned">{{$cart['prix']}} &euro;</td>
 				</tr>
 				@endforeach
-	{{dd($carts)}}
 				<tr>
 					<td></td>
 					<td></td>
@@ -27,9 +26,9 @@
 				</tr>
 			</tbody>
 		</table>
-		<form action="/cart/commade/{{id}}" method="post" >
+		<form action="/cart/command" method="post" >
 			{{ csrf_field() }}
-			<button type="submit" class="ui yellow button right floated">Commander</button>
+			<button type="submit" class="ui yellow button right floated" formmethod="post" formaction="/cart/command">Commander</button>
 		</form>
 		<form action="/cart/delete" method="post" accept-charset="utf-8">
 			{{ csrf_field() }}
